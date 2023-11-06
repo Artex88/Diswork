b04a8c30a59668f4e1253b44dd48253ca2bd5c75
Artex88 <zadevalov.alex@gmail.com>
2023-11-06 19:26:45 +0500

    Create custom login page; Create RegistrationService.

---
+++ src/main/java/ru/naumenJavaCourse/WebProject/Diswork/security/RegistrationService.java
@@ -0,0 +1,27 @@
+package ru.naumenJavaCourse.WebProject.Diswork.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.naumenJavaCourse.WebProject.Diswork.models.User;
+import ru.naumenJavaCourse.WebProject.Diswork.repositories.UserRepository;
+
+@Service
+public class RegistrationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+    }
+}
