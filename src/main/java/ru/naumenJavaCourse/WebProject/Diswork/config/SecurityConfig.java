16a481412b8599c70e012b38e2a6407294d4f652
Artex88 <zadevalov.alex@gmail.com>
2023-11-06 18:53:26 +0500

    Configure SecurityConfig.

--- src/main/java/ru/naumenJavaCourse/WebProject/Diswork/config/SecurityConfig.java
+++ src/main/java/ru/naumenJavaCourse/WebProject/Diswork/config/SecurityConfig.java
@@ -1,6 +1,35 @@
 package ru.naumenJavaCourse.WebProject.Diswork.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.naumenJavaCourse.WebProject.Diswork.services.UserDetailService;
+
 public class SecurityConfig {
-    // TODO
-    // Нужно реализовать аутентификацию
+
+    private final UserDetailService userDetailService;
+
+    public SecurityConfig(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        // конфигурируем авторизацию
+        return http.build();
+    }
 }
