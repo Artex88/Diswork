30dbf4670718c7c5812910cc1f746365b0b95dff
Artex88 <zadevalov.alex@gmail.com>
2023-11-06 14:42:00 +0500

    Initialize SpringBootApp with dependencies; Configure database

---
+++ src/main/java/ru/naumenJavaCourse/WebProject/Diswork/DisworkApplication.java
@@ -0,0 +1,13 @@
+package ru.naumenJavaCourse.WebProject.Diswork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/naumenJavaCourse/WebProject/Diswork/DisworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumenJavaCourse.WebProject.Diswork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
